2425061a6f387e542f3c1bf1e84d09219df93c13
chaosparadox <108727862+chaosparadox@users.noreply.github.com>
2022-12-19 22:39:30 +0300

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -3,12 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  // apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  // authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  // projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  // storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  // messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  // appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyB6or0YRE40JVLFRjeGawO2mKeAROPPLe0",
+  authDomain: "expro-app.firebaseapp.com",
+  projectId: "expro-app",
+  storageBucket: "expro-app.appspot.com",
+  messagingSenderId: "332085300360",
+  appId: "1:332085300360:web:6402b94570bbc456ffe33d",
 };
 
 const app = initializeApp(firebaseConfig);
